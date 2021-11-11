d2460536af4f458a44644fcb7b6ae8d1ccee89f0
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-11 15:09:42 +0330

    Add loan section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Styles from "./styles/Main.module.css";
 import Navbar from "./components/Navbar/";
-import NotFound from "./pages/NotFound/"
+import NotFound from "./pages/NotFound/";
 import Home from "./pages/Home/";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Loan from "./pages/Loan";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Loan />
+            </>
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
