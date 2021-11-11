c260645eef443fcd4b19de3c0ad321315a19855f
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-11 18:29:36 +0330

    Add external links support

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import Styles from "./Button.module.css";
 import { Link } from "react-router-dom";
 
-function Button({ children, path, action }) {
+function Button({ children, path, href, action }) {
   return (
     <Link to={path ? path : ""}>
-      <button className={Styles.button} onClick={action && action}>
+      <button
+        className={Styles.button}
+        onClick={(action && action, href && (() => window.open(href)))}
+      >
         {children}
       </button>
     </Link>
