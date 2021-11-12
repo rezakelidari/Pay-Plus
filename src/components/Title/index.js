970dfa634a0fdf2415f93276c7bc5b142664158a
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-12 14:30:15 +0330

    Add new features now title supports soBig size now title can be not-clickable

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Styles from "./Title.module.css";
 
-function Title({ big }) {
+function Title({ big, soBig, dark, clickable = true }) {
   return (
-    <Link to="/">
-      <h1 className={`${Styles.titleHeading} ${big && Styles.titleBig}`}>
+    <Link to={`${clickable ? "/" : ""}`}>
+      <h1
+        className={`
+          ${Styles.titleHeading}
+          ${big && Styles.titleBig}
+          ${soBig && Styles.titleSoBig}
+          ${dark && Styles.titleDark}`}
+      >
         Pay<span className={Styles.titleSubheading}>+</span>
       </h1>
     </Link>
