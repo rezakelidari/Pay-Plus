12b8a4174ff7066313ea4ebed72618269173ae1b
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-12 17:54:04 +0330

    Delete unwanted components

--- src/components/Wrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Styles from "./Wrapper.module.css";
-
-function Wrapper({ children }) {
-  return <div className={Styles.wrapper}>{children}</div>;
-}
-
-export default Wrapper;
