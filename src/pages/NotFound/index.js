0d3106e45023abbd23728d59c50550d81442ac71
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-13 13:08:15 +0330

    Make NotFound responsive

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -4,9 +4,9 @@ import Styles from "./Notfound.module.css";
 function NotFound() {
   return (
     <>
-      <div className={Styles.notfound}>
-        <h1 className={Styles.title}>Oops!</h1>
-        <p className={Styles.desc}>404 - Page not found...</p>
+      <div className={Styles.notfoundMain}>
+        <h1 className={Styles.notfoundTitle}>Oops!</h1>
+        <p className={Styles.notfoundDesc}>404 - Page not found...</p>
       </div>
     </>
   );
